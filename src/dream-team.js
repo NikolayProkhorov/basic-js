dbd3d46d7bf91df68a43510c0fac805bb2183ddb
NikolayProkhorov <79650551+NikolayProkhorov@users.noreply.github.com>
2022-05-02 18:43:01 +0300

    Add files via upload

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(array) {
+  // throw new NotImplementedError('Not implemented');
+  let x = [];
+  let y = [];
+  if (Array.isArray(array) === true) {
+    for (i = 0; i < array.length; i++) {
+      if (typeof (array[i]) == "string") {
+        x.push(array[i])
+      }
+    }
+    for (i = 0; i < x.length; i++) {
+      if (x[i] == ' ') {
+        x.splice(i, 1)
+      }
+    }
+    for (i = 0; i < x.length; i++) {
+      let z = x[i].toUpperCase().split('');
+      y.push(z[0]);
+    }
+    y.sort();
+    let nameTeam = y.join('');
+    return nameTeam
+   } else {
+    return false
+  }
 }
 
 module.exports = {
